0a3908d361a2245ba9d04e9c3632ea202e915a59
Slotheatleaf <162087909+Slotheatleaf@users.noreply.github.com>
2024-03-10 04:02:29 +0900

    Update print_hello_3.py

--- print_hello_3.py
+++ print_hello_3.py
@@ -1,1 +1,27 @@
-# test
+print('Hello World!')
+print('Hello World!')
+Print('Hello World!')
+
+print('Hello World!')
+print('Hello', 'World!')
+print('Hello'+'World!')
+print(100)
+print(2+4)
+
+100
+10+20
+10-20
+10*20
+10 / 20
+123 * 8765
+10+20*30
+
+x=100
+y=200
+x+y
+x-y
+x*y
+x/y
+x**2
+n=5
+n**2
